787d50e309ef8d64fd8491c25f90986db010cb5b
Moritz344 <moritzmaier34@gmail.com>
2025-08-02 15:52:45 +0200

    changed routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { MangaDetailsComponent } from './manga-details/manga-details.component';
 import { MangaBox1Component } from './manga-box1/manga-box1.component';
 import { StartPageComponent } from './start-page/start-page.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
 
+    { path: 'about',component: AboutComponent}, // about
     { path: '',component: StartPageComponent}, // Startseite
     { path: 'manga/suche',component: MangaBox1Component}, // Startseite
     { path: 'manga/:title',component: MangaDetailsComponent}, // Manga-details
