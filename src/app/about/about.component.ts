b744fb2d266c85a078fa182b6a287788a17d67fc
Moritz344 <moritzmaier34@gmail.com>
2025-08-02 15:49:21 +0200

    about page

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule, ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-about',
+  imports: [RouterModule],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.css'
+})
+export class AboutComponent {
+
+}
